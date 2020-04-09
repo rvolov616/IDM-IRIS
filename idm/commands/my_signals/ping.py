6737cff1b6492163c43e66a2ecdfd95d0b280de5
rvolov616 <63092676+rvolov616@users.noreply.github.com>
2020-04-09 19:47:50 +0600

    Update ping.py

--- idm/commands/my_signals/ping.py
+++ idm/commands/my_signals/ping.py
@@ -12,9 +12,16 @@ def ping(event: MySignalEvent) -> str:
     v_time_str = str(datetime.fromtimestamp(round(event.msg['date'])))
 
     r_type = 'ПОНГ' if event.command == "пинг" else "ПАУ" if event.command == "пиу" else "КОНГ"
-    message = f"""{r_type}
 
+    if delta > 15:r_type += "\nТРЕВОГА. ШИНАПРОВОД СГРЫЗЕН!!!!"
+    elif delta > 10:r_type += "\nТак кто-то конкретно грызет шинапровод."
+    elif delta > 5:r_type += "\nТак кто-то начинает грызть шинапровод."
+    else:r_type += "\neee сытый, спасибо что накормили его."
+
+    message = f"""{r_type}
     Ответ через: {delta} с.
+    Время сервера VK:  {v_time_str}
+    Время сервера IDM: {c_time_str}
     """.replace('    ', '')
     new_message(event.api, event.chat.peer_id, message=message)
 
